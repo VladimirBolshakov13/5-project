92a47b3021c6d00021bdb88c16274cec8a7c4ffd
VladimirBolshakov13 <157284825+VladimirBolshakov13@users.noreply.github.com>
2024-02-27 21:43:48 +0300

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,38 +1,42 @@
-# 108200943
-def decode_instructions(s: str) -> str:
+# 108273025
+BASE_TEN = 10
+OPEN_BRACKET = "["
+CLOSE_BRACKET = "]"
+
+
+def decode_instructions(encoded_instructions: str) -> str:
     """
-    Декодирует строку, представляющую набор инструкций, закодированных в определенном формате.
+    Декодирует закодированную инструкцию и возвращает раскодированную строку.
 
-    Входная строка s содержит инструкции, закодированные следующим образом:
-    - Цифры представляют количество повторений подстроки.
-    - Квадратные скобки [] указывают на секцию инструкций, которую нужно повторить.
+    Аргументы:
+    encoded_instructions (str): Закодированная инструкция, содержащая цифры, скобки и символы.
 
-    Args:
-        s: Строка, содержащая закодированные инструкции.
+    Возвращает:
+    str: Раскодированная строка на основе закодированной инструкции.
 
-    Returns:
-        Раскодированные инструкции в виде строки.
-
-    Example:
-        Input: "3[a]2[bc]"
-        Output: "aaabcbc"
-
+    Пример использования:
+    encoded_instructions = "3[a]2[bc]"
+    decoded_instructions = decode_instructions(encoded_instructions)
+    print(decoded_instructions)
+    "aaabcbc"
     """
-    stack = []
+
+    stack: list[str] = []  # Список для хранения элементов стека
     num = 0
     temp_str = ""
-    for char in s:
-        if char.isdigit():
-            num = num * 10 + int(char)
-        elif char == "[":
+    for char in encoded_instructions:
+        if char in "0123456789":
+            num = num * BASE_TEN + int(char)
+        elif char == OPEN_BRACKET:
             stack.append((temp_str, num))
             temp_str = ""
             num = 0
-        elif char == "]":
+        elif char == CLOSE_BRACKET:
             prev_str, repeat_times = stack.pop()
             temp_str = prev_str + temp_str * repeat_times
         else:
             temp_str += char
+
     return temp_str
 
 
